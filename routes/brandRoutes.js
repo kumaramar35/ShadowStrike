9f0ab23a8f4053d82d3859e1d44756328bd004d9
kumaramar35 <43752742+kumaramar35@users.noreply.github.com>
2025-09-25 01:28:18 +0530

    Update brandRoutes.js

--- routes/brandRoutes.js
+++ routes/brandRoutes.js
@@ -10,16 +10,9 @@ import { authorizeRoles } from "../middlewares/roleMiddleware.js";
 
 const router = express.Router();
 
-// Get all brands
 router.get("/", isAuthenticated, authorizeRoles("admin", "super_admin"), getBrands);
-
-// Get a single brand
 router.get("/:id", isAuthenticated, authorizeRoles("admin", "super_admin"), getBrandById);
-
-// Create brand
 router.post("/", isAuthenticated, authorizeRoles("admin", "super_admin"), createBrand);
-
-// Update brand
 router.put("/:id", isAuthenticated, authorizeRoles("admin", "super_admin"), updateBrand);
 
 export default router;
