6f22b508b16b4008ca2b7428730b8e67f8f4c701
kumaramar35 <43752742+kumaramar35@users.noreply.github.com>
2025-09-25 01:29:17 +0530

    Update profileRoutes.js

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -4,10 +4,10 @@ import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// Get user profile
+
 router.get("/", isAuthenticated, getProfile);
 
-// Update user profile
+
 router.put("/", isAuthenticated, updateProfile);
 
 export default router;
