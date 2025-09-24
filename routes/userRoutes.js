acadb32e21b16347e9b5bf2b31035008e05f124b
kumaramar35 <43752742+kumaramar35@users.noreply.github.com>
2025-09-25 01:33:27 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,10 @@ import {
   getAllUsers, 
   getUserById, 
   updateUser, 
-  deleteUser 
+  deleteUser ,
+  listUsers,
+  impersonateUser,
+  stopImpersonation
 } from "../controllers/userController.js";
 
 
@@ -13,11 +16,15 @@ import { authorizeRoles } from "../middlewares/roleMiddleware.js";
 const router = express.Router();
 
 // Only super_admin can manage users
-router.use(isAuthenticated, authorizeRoles("super_admin"));
-
+router.use(isAuthenticated, authorizeRoles("admin", "super_admin"));
+router.get("/userList", listUsers);
+router.post("/impersonate/:userId", impersonateUser);
+router.post("/stop-impersonation", stopImpersonation);
 router.get("/", getAllUsers);
 router.get("/:id", getUserById);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
+
+
 
 export default router;
